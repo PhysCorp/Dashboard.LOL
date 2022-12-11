c4252a3c21b2b8c08aba96d334e79c2a4e288fc8
Matt Curtis <physicallyrendered@gmail.com>
2022-12-10 21:17:15 -0500

    Fixed db_setup.sql

--- setup/db_setup.sql
+++ setup/db_setup.sql
@@ -1,27 +1,6 @@
 CREATE DATABASE dashboard_lol if not exists;
 USE dashboard_lol;
 
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
---
--- Host: localhost    Database: dashboard_lol
--- ------------------------------------------------------
--- Server version	8.0.30
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `added`
---
-
 DROP TABLE IF EXISTS `added`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -37,7 +16,7 @@ CREATE TABLE `added` (
   KEY `fk_Added_Widgets1_idx` (`widget_id`),
   CONSTRAINT `fk_Added_Users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   CONSTRAINT `fk_Added_Widgets1` FOREIGN KEY (`widget_id`) REFERENCES `widgets` (`widget_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,7 +39,7 @@ CREATE TABLE `appstore` (
   UNIQUE KEY `widget_id_UNIQUE` (`widget_id`),
   KEY `fk_AppStore_Widgets1_idx` (`widget_id`),
   CONSTRAINT `fk_AppStore_Widgets1` FOREIGN KEY (`widget_id`) REFERENCES `widgets` (`widget_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,6 +58,7 @@ CREATE TABLE `users` (
   `user_last_name` varchar(255) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_phone_num` varchar(255) NOT NULL,
+  `user_data` longtext,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`),
   UNIQUE KEY `user_name_UNIQUE` (`user_name`)
@@ -99,20 +79,7 @@ CREATE TABLE `widgets` (
   PRIMARY KEY (`widget_id`),
   UNIQUE KEY `widget_id_UNIQUE` (`widget_id`),
   UNIQUE KEY `internal_name_UNIQUE` (`internal_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb3;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-12-07 19:41:50
-
--- Create a new user called "dashboard_agent" with the password "thedashboardliveson" and grant it access to the database "dashboard_lol"
-CREATE USER 'dashboard_agent'@'localhost' IDENTIFIED BY 'thedashboardliveson';
+CREATE USER 'dashboard_agent'@'localhost' IDENTIFIED BY 'thedashboardliveson' if not exists;
 GRANT ALL PRIVILEGES ON dashboard_lol.* TO 'dashboard_agent'@'localhost';
